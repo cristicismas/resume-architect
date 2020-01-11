59c76f5e26054b3d865fc1f7519eb340ffe7e206
Cristi Cismas <cristicismas99@gmail.com>
2020-01-11 12:43:02 +0200

    Refactor handlers/template.ts

--- server/src/handlers/template.ts
+++ server/src/handlers/template.ts
@@ -5,11 +5,13 @@ import { IResume } from '../interfaces/resume';
 export const getTemplateLinks = async () => {
   const templates = await cloudinary.v2.api.resources({ type: 'upload', prefix: 'resume_architect/' });
 
-  // Returns all template links with a .png extension in an array
+  // Returns all template links with a .png extension
   return templates.resources.map((pdf: IResume) => {
     return {
       name: pdf.public_id,
-      url: `${pdf.secure_url.slice(0, -4)}.png`
+      url: getPngURL(pdf.secure_url)
     };
   });
 };
+
+const getPngURL = originalURL => `${originalURL.slice(0, -4)}.png`;

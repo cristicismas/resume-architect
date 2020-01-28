61f520fe99681149cac90b1483270e4c7335b1b8
Cristi Cismas <cristicismas99@gmail.com>
2020-01-28 15:56:23 +0200

    Update buildResume schema

--- client/src/schemas/buildResume.js
+++ client/src/schemas/buildResume.js
@@ -1,17 +1,18 @@
 import * as Yup from 'yup';
 
 const jobsSchema = Yup.object().shape({
+  company: Yup.string(),
   job: Yup.string(),
-  jobStartDate: Yup.date(),
-  jobEndDate: Yup.date(),
+  jobStartDate: Yup.date().nullable(),
+  jobEndDate: Yup.date().nullable(),
   responsibilities: Yup.string()
 });
 
 const schoolsSchema = Yup.object().shape({
   school: Yup.string(),
   degree: Yup.string(),
-  schoolStartDate: Yup.date(),
-  shoolEndDate: Yup.date()
+  schoolStartDate: Yup.date().nullable(),
+  schoolEndDate: Yup.date().nullable()
 });
 
 export const buildResumeSchema = Yup.object().shape({
@@ -22,7 +23,6 @@ export const buildResumeSchema = Yup.object().shape({
     .email('The email format is not valid. Are you sure you entered your email correctly?')
     .required('Please add your email.'),
   about: Yup.string().required('Please complete this field.'),
-  company: Yup.string(),
   jobs: Yup.array().of(jobsSchema),
   schools: Yup.array().of(schoolsSchema),
   extra: Yup.string()

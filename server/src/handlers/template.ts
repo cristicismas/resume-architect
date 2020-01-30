9953c1231046a5dd203bc3fe2dd63c67cf8256e0
Cristi Cismas <cristicismas99@gmail.com>
2020-01-30 12:20:03 +0200

    Refactor template.ts to use async await instead of callbacks and promises

--- server/src/handlers/template.ts
+++ server/src/handlers/template.ts
@@ -1,45 +1,44 @@
 import { Request } from 'hapi';
 import { ITemplatePreview } from '../interfaces/template';
-import * as fs from 'fs';
+import * as fs from 'fs-extra';
+import * as Boom from '@hapi/boom';
 
 const max_results = 6;
 
 export const getTemplatePreviewsLinks = async (request: Request) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile('template_previews.json', 'UTF8', (err, content) => {
-      if (err) reject(err);
-      else {
-        const { indexToFetch } = request.params;
+  try {
+    const linksFile = await fs.readFile('template_previews.json', 'UTF8');
+    const { indexToFetch } = request.params;
 
-        let templateLinks = JSON.parse(content);
+    let templateLinks = JSON.parse(linksFile);
 
-        if (indexToFetch + max_results > templateLinks.length) {
-          templateLinks = templateLinks.slice(indexToFetch, templateLinks.length);
-        } else {
-          templateLinks = templateLinks.slice(indexToFetch, indexToFetch + max_results);
-        }
+    if (indexToFetch + max_results > templateLinks.length) {
+      templateLinks = templateLinks.slice(indexToFetch, templateLinks.length);
+    } else {
+      templateLinks = templateLinks.slice(indexToFetch, indexToFetch + max_results);
+    }
 
-        resolve(templateLinks);
-      }
-    });
-  });
+    return templateLinks;
+  } catch (err) {
+    console.log(err);
+    return Boom.badImplementation('Something went wrong getting the template preview links');
+  }
 };
 
 export const getSinglePreviewLink = async (request: Request) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile('template_previews.json', 'UTF8', (err, content) => {
-      if (err) reject(err);
-      else {
-        const { templateName } = request.params;
+  try {
+    const linksFile = await fs.readFile('template_previews.json', 'UTF8');
+    const { templateName } = request.params;
 
-        let templateLinks = JSON.parse(content);
+    let templateLinks = JSON.parse(linksFile);
 
-        const template = templateLinks.find((template: ITemplatePreview) => {
-          if (template.name === templateName) return true;
-        });
-
-        resolve(template);
-      }
+    const template = templateLinks.find((template: ITemplatePreview) => {
+      if (template.name === templateName) return true;
     });
-  });
+
+    return template;
+  } catch (err) {
+    console.log(err);
+    return Boom.badImplementation('Something went wrong getting the preview link');
+  }
 };

1ff34d062fffce29eee762e98a744ef86c9903dd
Cristi Cismas <cristicismas99@gmail.com>
2020-02-14 12:50:31 +0200

    Make the datepicker fields portals (modals) and style them

--- client/src/components/form/DatePickerField.js
+++ client/src/components/form/DatePickerField.js
@@ -3,10 +3,11 @@ import DatePicker from 'react-datepicker';
 import './DatePickerField.css';
 import 'react-datepicker/dist/react-datepicker.css';
 
-const DatePickerField = ({ namePrefix, startDate, endDate, onChange, monthYearPicker=false }) => {
+const DatePickerField = ({ namePrefix, startDate, endDate, onChange, monthYearPicker = false }) => {
   return (
     <div className="dates-group">
       <DatePicker
+        withPortal
         selected={startDate ? new Date(startDate) : null}
         onChange={date => onChange(`${namePrefix}StartDate`, date)}
         selectsStart
@@ -21,6 +22,7 @@ const DatePickerField = ({ namePrefix, startDate, endDate, onChange, monthYearPi
       <span className="date-separator" />
 
       <DatePicker
+        withPortal
         todayButton="Present"
         selected={endDate ? new Date(endDate) : null}
         onChange={date => onChange(`${namePrefix}EndDate`, date)}

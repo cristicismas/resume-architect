ac0669d6642285eefacc038946cdf130ed104348
Cristi Cismas <cristicismas99@gmail.com>
2020-03-11 16:15:18 +0200

    Make the restoreScroll bool in useScrollToTop default to true

--- client/src/hooks/useScrollToTop.js
+++ client/src/hooks/useScrollToTop.js
@@ -2,10 +2,9 @@ import { useEffect } from 'react';
 
 // Scrolls to the top of the page on mount.
 // Should be used on every route that needs scroll restoration.
-const useScrollToTop = (restoreScroll) => {
+const useScrollToTop = (restoreScroll = true) => {
   useEffect(() => {
-    if (restoreScroll)
-      window.scrollTo(0, 0);
+    if (restoreScroll) window.scrollTo(0, 0);
   }, [restoreScroll]);
 };
 

11daf90b6419e3e4cf101ce2278c801fd98af7e5
Cristi Cismas <cristicismas99@gmail.com>
2020-03-15 13:56:11 +0200

    Edit current tips to use line breaks, and modify some copy for lisibility

--- client/src/constants/tips.js
+++ client/src/constants/tips.js
@@ -1,19 +1,19 @@
 export default {
   name: {
     title: 'Full Name',
-    text: 'Your full name. Pretty self-explanatory if you ask me.'
+    text: 'Your full name. Hopefully you don\'t need a tip for this.'
   },
   address: {
     title: 'Address',
-    text: 'Where you currently live. Street, city, region and/or postal code if you wish to include it.'
+    text: 'Where you currently live.<br />Usually includes street, city, region and/or postal code.'
   },
   phone: {
     title: 'Phone Number',
-    text: 'Your active phone number. Expect to receive calls on this number after sending your resumes. Include a prefix if you wish to work abroad.'
+    text: 'Your active phone number.<br />Expect to receive calls on this number after sending your resume. Include your country prefix if you wish to work abroad.'
   },
   email: {
     title: 'Email',
-    text: 'Your personal email address. Expect to receive emails to this address after sending your resumes. The email should follow this format: youremail@example.com'
+    text: 'Your personal email address.<br />Expect to receive emails to this address after sending your resume. The email should follow this format: youremail@example.com'
   },
   about: {
     title: 'title',

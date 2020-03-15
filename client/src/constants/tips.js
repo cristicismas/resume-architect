fe3a5d332347778f2426b9850601662b33f1da86
Cristi Cismas <cristicismas99@gmail.com>
2020-03-15 14:09:45 +0200

    Add copy for the missing tips

--- client/src/constants/tips.js
+++ client/src/constants/tips.js
@@ -16,39 +16,39 @@ export default {
     text: 'Your personal email address.<br />Expect to receive emails to this address after sending your resume. The email should follow this format: youremail@example.com'
   },
   about: {
-    title: 'title',
-    text: 'text'
-  },
-  extra: {
-    title: 'title',
-    text: 'text'
+    title: 'About',
+    text: 'A few words about yourself.<br />Include things that you are passionate about, a short history, or skills you have that might help you in your future job. Try to keep it short.'
   },
   jobCompany: {
-    title: 'title',
-    text: 'text'
+    title: 'Company',
+    text: 'The full name of the company.'
   },
   jobTitle: {
-    title: 'title',
-    text: 'text'
+    title: 'Job Title',
+    text: 'The position you occupied in the company / your job title. For example: Human Resources Intern / Lead Web Developer.'
   },
   jobDate: {
-    title: 'title',
-    text: 'text'
+    title: 'Date',
+    text: 'The date you started working there and the date you stopped. Approximations are alright, too.'
   },
   jobResponsibilities: {
-    title: 'title',
-    text: 'text'
+    title: 'Responsibilities',
+    text: 'The role you played in the company\'s success. Try to have some statistics here (Improved x by 16% doing y). Having bullet points here is recommended.'
   },
   school: {
-    title: 'title',
-    text: 'text'
+    title: 'School',
+    text: 'Your college / university / high school name.'
   },
   schoolDegree: {
-    title: 'title',
-    text: 'text'
+    title: 'Degree',
+    text: 'The degree you\'ve obtained following the courses.'
   },
   schoolDate: {
-    title: 'title',
-    text: 'text'
+    title: 'Date',
+    text: 'The date you started studying there and the date you stopped.'
+  },
+  extra: {
+    title: 'Extra',
+    text: 'Anything you would like to add to the end of your resume.<br />This could include other certifications / diplomas, special skills, volunteering, etc.'
   }
 };

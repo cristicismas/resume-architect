8261af4d0a88737785993e7ff5e2d834b9470174
Cristi Cismas <cristicismas99@gmail.com>
2020-04-07 19:35:27 +0300

    Change the buildResumeSchema to allow optional _id properties

--- server/src/constants/schemas.ts
+++ server/src/constants/schemas.ts
@@ -1,31 +1,32 @@
 import Joi from '@hapi/joi';
 
 const buildResumeSchema = Joi.object({
+  _id: Joi.string().optional(),
   name: Joi.string().required(),
   address: Joi.string().allow(''),
   phoneNumber: Joi.string().allow(''),
-  email: Joi.string()
-    .email()
-    .required(),
+  email: Joi.string().email().required(),
   about: Joi.string().required(),
   jobs: Joi.array().items(
     Joi.object({
+      _id: Joi.string().optional(),
       company: Joi.string().allow(''),
       job: Joi.string().allow(''),
       jobStartDate: Joi.date(),
       jobEndDate: Joi.date(),
-      responsibilities: Joi.string()
+      responsibilities: Joi.string(),
     })
   ),
   schools: Joi.array().items(
     Joi.object({
+      _id: Joi.string().optional(),
       school: Joi.string().allow(''),
       degree: Joi.string().allow(''),
       schoolStartDate: Joi.date(),
-      schoolEndDate: Joi.date()
+      schoolEndDate: Joi.date(),
     })
   ),
-  extra: Joi.string().allow('')
+  extra: Joi.string().allow(''),
 });
 
 export default { buildResumeSchema };

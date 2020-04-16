0bdb4f65a48dd6a4c124654edc2cff9de0847ca4
Cristi Cismas <cristicismas99@gmail.com>
2020-04-16 10:04:26 +0300

    Fix a bug where all icons have undefined className

--- client/src/components/misc/Icon.js
+++ client/src/components/misc/Icon.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 const Icon = ({ icon, size, className, ...otherProps }) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" className={`icon ${className}`} width={size} height={size} viewBox="0 0 24 24" {...otherProps}>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={`icon ${className ? className : ''}`}
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      {...otherProps}>
       <path d={icon} />
     </svg>
   );

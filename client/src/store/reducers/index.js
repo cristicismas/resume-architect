03de0452e2879eb9a06f706b70ffcf497204891c
Cristi Cismas <cristicismas99@gmail.com>
2020-05-06 17:58:37 +0300

    Clear redux store on LOGGED_OUT action

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -1,11 +1,20 @@
 import { combineReducers } from 'redux';
+import { LOGGED_OUT } from '../actionTypes';
 
 import previews from './previews';
 import resume from './resume';
 import user from './user';
 
-export default combineReducers({
+const allReducers = combineReducers({
   previews,
   resume,
   user
 });
+
+export default (state, action) => {
+  if (action.type === LOGGED_OUT) {
+    state = undefined;
+  }
+
+  return allReducers(state, action);
+};

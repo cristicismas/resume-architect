0e3f1c525d2022b8cb882cc43e7c7bbd9b5adb6f
Cristi Cismas <cristicismas99@gmail.com>
2020-05-10 18:50:01 +0300

    Refactor PrivateRoute to also accept other props and pass them to the Route component

--- client/src/components/routes/PrivateRoute.js
+++ client/src/components/routes/PrivateRoute.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
 
-const PrivateRoute = ({ path, redirect, children }) => {
+const PrivateRoute = ({ path, redirect, children, ...otherProps }) => {
   const { loggedIn, tokenChecked } = useSelector(state => state.user);
 
   if (tokenChecked) {
     return (
-      <Route path={path}>
+      <Route {...otherProps} path={path}>
         {loggedIn ? children : <Redirect to={redirect ? redirect : '/login'} />}
       </Route>
     );

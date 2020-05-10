9fc0a8fed16e8c85fdc175704d43c62323f58073
Cristi Cismas <cristicismas99@gmail.com>
2020-05-10 18:09:29 +0300

    Prevent window close or refresh on some routes

--- client/src/components/routes/RouteChangeHandler.js
+++ client/src/components/routes/RouteChangeHandler.js
@@ -1,19 +1,48 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 
+// Remove all selection on route change
+// This is needed because when exiting a modal, text is sometimes selected.
+const removeSelection = () => {
+  if (window.getSelection().empty) {
+    // Chrome
+    window.getSelection().empty();
+  } else if (window.getSelection().removeAllRanges) {
+    // Firefox
+    window.getSelection().removeAllRanges();
+  }
+};
+
+const preventWindowRefreshOrClose = pathname => {
+  const routesToPrevent = ['/change_template', '/save'];
+
+  let shouldPrevent = false;
+
+  for (const route of routesToPrevent) {
+    if (pathname.includes(route)) {
+      shouldPrevent = true;
+      break;
+    }
+  }
+
+  if (shouldPrevent) {
+    window.onbeforeunload = e => {
+      e.preventDefault();
+      return 'Are you sure you want to close?';
+    };
+  } else {
+    window.onbeforeunload = () => {};
+  }
+};
+
 const RouteChangeHandler = () => {
   const history = useHistory();
 
-  // Remove all selection on route change
-  // This is mostly needed because when exiting a modal, some text is sometimes selected.
-  history.listen(() => {
-    if (window.getSelection().empty) {
-      // Chrome
-      window.getSelection().empty();
-    } else if (window.getSelection().removeAllRanges) {
-      // Firefox
-      window.getSelection().removeAllRanges();
-    }
+  history.listen(location => {
+    const { pathname } = location;
+
+    removeSelection();
+    preventWindowRefreshOrClose(pathname);
   });
 
   return <>{null}</>;

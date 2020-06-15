747b8986236add102d1d4ae13f772755f0e12a87
Cristi Cismas <cristicismas99@gmail.com>
2020-06-15 18:39:55 +0300

    Add icons to the Features cards titles

--- client/src/components/layout/Features.js
+++ client/src/components/layout/Features.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import Icon from '../misc/Icon';
 import './Features.css';
+
+import ICONS from '../../constants/icons';
 
 const Features = () => {
   return (
@@ -8,7 +11,10 @@ const Features = () => {
 
       <div className="feature-cards">
         <div className="card">
-          <h3 className="card-title">Easy to use</h3>
+          <h3 className="card-title">
+            <Icon className="suitcase-icon" icon={ICONS.SUITCASE} fill="#111" size={30} /> Easy to use
+          </h3>
+
           <p className="card-description">
             Your job hunt doesn't need to be any more complicated than it already is. This is a free, easy-to-use resume
             builder. Simply pick a template, fill a form, and download your resume!
@@ -16,7 +22,11 @@ const Features = () => {
         </div>
 
         <div className="card">
-          <h3 className="card-title">Completely free</h3>
+          <h3 className="card-title">
+            <Icon className="money-icon" icon={ICONS.MONEY} fill="#111" size={30} />
+            Completely free
+          </h3>
+
           <p className="card-description">
             This is a completely free service, that helps you build, customise, and save your resume with ease. There
             are no subscriptions or hidden paywalls a that you hit at the end of your form.
@@ -24,7 +34,11 @@ const Features = () => {
         </div>
 
         <div className="card">
-          <h3 className="card-title">We'll help you out</h3>
+          <h3 className="card-title">
+            <Icon className="tip-icon" icon={ICONS.TIP} fill="#111" size={30} />
+            We'll help you out
+          </h3>
+
           <p className="card-description">
             No idea what to write? No problem. We tell you exactly what you need to fill out and give you useful
             examples. Just pick the design you like and get started! You'll be done in no time.
